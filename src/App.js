c7cc7b4804d0592c032f8d464c6da7bbcc597578
HNH1105 <137039470+HNH1105@users.noreply.github.com>
2023-06-22 22:40:46 +0700

    test
    test

---
+++ src/App.js
@@ -0,0 +1,381 @@
+
+
+
+function App() {
+  return (
+
+<div className="App"   >
+<img style={{
+      
+      zIndex:'-1',  
+      position: 'absolute',  left: '263px'
+    }}src="designer_1.png"></img>
+<div >
+
+    <label style={{marginTop:'18px', height:'24px',width: '38px',marginLeft:'165px',
+styleName: 'Regular 14',
+fontFamily: 'Roboto',
+fontSize: '14px',
+fontWeight: 400,
+lineHeight: '24px',
+letterSpacing: '0px',
+textAlign: 'left',
+  }}>Home</label>
+    <label style={{ height:'24px',width: '38px',marginLeft:'60px',
+  styleName: 'Regular 14',
+fontFamily: 'Roboto',
+fontSize: '14px',
+fontWeight: 400,
+lineHeight: '24px',
+letterSpacing: '0px',
+textAlign: 'left',
+  }}> About</label>
+    <label style={{height:'49px',width: '24px',marginLeft:'60px',
+  styleName: 'Regular 14',
+fontFamily: 'Roboto',
+fontSize: '14px',
+fontWeight: 400,
+lineHeight: '24px',
+letterSpacing: '0px',
+textAlign: 'left', }}>Contact</label> 
+    <label style={{  
+    position:'absolute',
+  marginLeft:'271px',
+  fontFamily: 'Roboto',
+  fontStyle: 'normal',
+  fontWeight: 900,
+  fontSize: '26px',
+  lineHeight: '38px',
+  marginTop:'10px',
+  color: '#37447E' }}>Landing</label>
+  <button style={{
+    height:'26px',width: '160px', 
+    marginLeft:'639px',background: '#111B47',
+    marginTop:'17px',
+    color: '#FFFFFF' }}>Buy Now</button>
+  </div>
+
+  <div style={{ marginTop:'261px'}}> 
+  <label style={{ height:'132px',width: '539px', marginRight:'1205px',marginLeft:'163px', 
+  fontFamily: 'Roboto',
+  fontStyle: 'normal',
+  fontWeight: 500,
+  fontSize: '50px',
+  lineHeight: '66px',
+  color: '#091133',
+  letterSpacing:'1px',
+  textAlign:'left'
+}} >Introduce Your Product Quickly & Effectively</label>
+  </div>
+  <div style={{marginLeft:'163px'  }}>
+    
+    
+  <label style={{
+     
+      height: '164px',
+      width: '539px',
+      marginTop:'20px',
+      marginRight:'1205px',
+      fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      fontSize: '18px',
+      lineHeight: '30px',
+      color: '#505F98',
+      textAlign:'left'
+  }}>
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 
+mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+    </label>
+    </div>
+    <div style={{ marginTop:'20px', marginLeft:'163px'}}>  
+      <button
+      style={{ 
+      height: '26px',
+      width: '189px',fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      fontSize: '16px',
+      lineHeight: '26px',
+      color: '#FFFFFF',
+      textAlign:'center',
+      background: '#111B47',
+      borderradius: '2px'}}
+      
+      > Purchase UI Kit</button>
+      <button style={{ marginLeft:'30px', 
+      height: '26px',
+      width: '189px',fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      fontSize: '16px',
+      lineHeight: '26px',
+      color: '#091133',
+      textAlign:'center',
+      background: '#E7ECFF',
+      opacity: 0.5}}> Learn More</button>
+    </div>
+   
+
+      
+      <div style={{marginLeft:'75px',marginTop:'551px'}}className="row col">
+
+      <div className="col-6"> 
+      <label style={{marginLeft:'63px',  height: '48px',
+      width: '540px',  
+       fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      color: '#091133',
+      fontWeight: 500,
+     fontSize: '36px',
+      lineHeight: '48px',
+      }}>Light, Fast & Powerful</label>
+      <div className="col">
+        <label style={{marginLeft:'63px'}} > Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+
+mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</label>
+    
+       </div>
+       <div style={{marginTop:'105px',marginLeft:'40px'}} className="row">
+       <div className="col-4"> <img style={{marginLeft:'35px'
+    
+    }}src="CombinedShape.png"></img>
+    <label style={{marginTop:'10px',color:'#091133',
+
+      fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      fontSize: '16px',
+      lineHeight: '26px'
+  
+  }}>Title Goes Here</label>
+    <label style={{marginTop:'10px'}} >Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </label>
+    </div>
+       <div  className="col-4"> <img style={{marginLeft:'35px'
+    }}src="CombinedShape.png"></img>
+     <label style={{marginTop:'10px', 
+       fontFamily: 'Roboto',
+       fontStyle: 'normal',
+       fontWeight: 500,
+       fontSize: '16px',
+       lineHeight: '26px'
+    }}>Title Goes Here</label>
+     <label style={{marginTop:'10px'}} >Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </label>
+    </div>
+       </div>
+
+      </div>
+      <div className="col-6"> 
+
+      <img style={{ display:"inline-block",
+      position:"absolute"
+    }}src="undraw_mobile_login_ikmv.png"></img>
+</div>
+      </div>
+      
+      
+
+
+      <div style={{marginTop:'150px',marginLeft:'105px'}} className="row">
+      <div className="col">
+      <div  style={{marginTop:'5px'}}className="col"> <img  src="anh1.png"></img></div>
+      <div  style={{marginTop:'100px'}}className="col"> <img  src="anh2.png"></img></div>
+      <div style={{marginTop:'100px'}} className="col"> <img  src="anh3.png"></img></div>
+      </div> 
+      <div className="col">
+      <div style={{marginTop:'5px'}} className="col"> 
+      <label  style={{ height: '48px', marginTop:'70px',
+      width: '540px',  
+       fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      color: '#091133',
+      fontWeight: 500,
+     fontSize: '36px',
+      lineHeight: '48px',
+      }}>Light, Fast & Powerful</label>
+      <label 
+      style={{   height: '48px', marginRight:'150px'}}
+      
+      >Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 
+mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</label>
+      </div>
+      <div  style={{marginTop:'240px'}}className="col">   <label  style={{ height: '48px',
+      width: '540px',  
+       fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      color: '#091133',
+      fontWeight: 500,
+     fontSize: '36px',
+      lineHeight: '48px',
+      }}>Light, Fast & Powerful</label>
+      <label 
+      style={{  height: '48px', marginRight:'150px'}}
+      
+      >Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 
+mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</label></div>
+      <div style={{marginTop:'270px'}} className="col">   <label  style={{ height: '48px',
+      width: '540px',  
+       fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      color: '#091133',
+      fontWeight: 500,
+     fontSize: '36px',
+      lineHeight: '48px',
+      }}>Light, Fast & Powerful</label>
+      <label 
+      style={{  height: '48px', marginRight:'200px', marginTop:'5px'}}
+      
+      >Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 
+mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</label></div>
+<button
+      style={{ 
+      height: '26px',
+      width: '189px',fontFamily: 'Roboto', 
+      fontStyle: 'normal',
+      fontWeight: 500,
+      fontSize: '16px',
+      lineHeight: '26px',
+      color: '#FFFFFF',
+      marginTop:'10px',
+      textAlign:'center',
+      background: '#111B47',
+      borderradius: '2px'}}
+      
+      > Purchase UI Kit</button>
+      </div>
+      </div>
+
+      <div style={{marginTop:'100px'}} className="row">
+
+        <div style={{}} className="col">
+          
+          <center><label  style={{ height: '48px',
+      width: '540px',  
+       fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      color: '#091133',
+      fontWeight: 500,
+     fontSize: '36px',
+      lineHeight: '48px',
+      }}>A Price To Suit Everyone</label></center>
+        </div>
+
+      <center><label style={{width:'600px',height:'78px'}}> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus </label></center>
+
+      </div>
+      <center><label style={{ height: '48px',
+      width: '540px',  
+       fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      color: '#091133',
+      fontWeight: 500,
+     fontSize: '36px',
+      lineHeight: '48px',
+      }} >$40</label></center>
+
+      <center>UI Design Kit</center>
+
+      <center><label style={{marginTop:'40px',
+styleName: 'Regular 14',
+fontFamily: 'Roboto',
+fontSize:'14px',
+fontWeight: '400',
+lineHeight: '24px',
+letterSpacing: '0px',
+textAlign:'center',
+
+    
+    }}>See, One price. Simple.</label></center>
+      <center><button
+      style={{ 
+      height: '26px',
+      width: '189px',fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      fontSize: '16px',
+      lineHeight: '26px',
+      color: '#FFFFFF',
+      textAlign:'center',
+      background: '#111B47',
+      borderradius: '2px'}}
+      
+      > Purchase UI Kit</button></center>
+    
+      <div style={{backgroundColor:'#E7ECFF',marginTop:'100px'}} className="row">
+      <hr></hr>
+      <div className='col-4'>
+        <center><label style={{
+        styleName: 'Medium 14',
+        fontFamily: 'Roboto',
+        fontSize: '14px',
+        fontWeight: 500,
+        lineheight: '24px',
+        letterSpacing: '0px',
+        textAlign: 'left',
+        marginTop:'80px',
+        marginRight:'17px'   
+        }}>@2023 Your Company</label></center>
+        </div>
+      <div className='col-4'><center>  <label style={{  
+
+            fontFamily: 'Roboto',
+            fontStyle: 'normal',
+            fontWeight: 900,
+            fontSize: '26px',
+            lineHeight: '38px',
+  color: '#37447E',
+  marginTop:'70px'
+ }}>Landing</label></center></div>
+      <div className='col-4'><center><button
+      style={{ 
+      height: '26px',
+      width: '109px',fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      fontSize: '12px',
+      lineHeight: '18px',
+      color: '#FFFFFF',
+      textAlign:'center',
+      background: '#111B47',
+        marginTop:'75px',
+      borderradius: '2px',
+       marginLeft:'40px', 
+       letterSpacing:'0px'
+    }}
+ 
+
+
+      > Purchase Now</button></center></div>
+      <center><hr style={{marginTop:'80px', width:'80%'}}></hr></center>  
+      <div className="col-1"></div>
+      <div className="col-2">
+        <span style={{marginLeft:'15px'}}>Home</span> 
+        <span style={{marginLeft:'30px'}}>About</span> 
+        <span style={{marginLeft:'30px'}}>Contact</span> 
+      </div>
+      <div className="col-5"></div>
+      <div className="col-4"> 
+      <img style={{marginLeft:'75px'
+     }}src="Path.png"></img>
+       <img style={{marginLeft:'35px'
+     }}src="in.png"></img>
+     <img style={{marginLeft:'35px'
+     }}src="aaa.png"></img>
+     <img style={{marginLeft:'35px'
+     }}src="Shape.png"></img>
+      <img style={{marginLeft:'35px'
+     }}src="Combined.png"></img>
+      </div>
+      <div style={{marginTop:'20px'}} className="row"></div>
+      </div>
+    </div>
+
+
+
+// page 2
+
+
+  );
+}
+
+export default App;
